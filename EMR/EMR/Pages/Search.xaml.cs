79dfc3556a45019ce38cad20d1dee4d913915633
elizaebthkuok <elizabethkuok29@gmail.com>
2019-02-21 12:51:55 -0700

    Add the UI of Search Page

---
+++ EMR/EMR/Pages/Search.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EMR.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Search : ContentPage
+	{
+		public Search ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Clicked_Advanced(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Clicked_SearchPatient(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
