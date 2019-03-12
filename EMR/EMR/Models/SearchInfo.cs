dc952de87f6f240ab403a36af296ed577d949cb3
elizaebthkuok <elizabethkuok29@gmail.com>
2019-03-11 21:58:15 -0600

    SearchInfo Constructors

--- EMR/EMR/Models/SearchInfo.cs
+++ EMR/EMR/Models/SearchInfo.cs
@@ -12,6 +12,55 @@ namespace EMR.Models
         private string lastName;
         private string firstName;
 
+        public int DOB
+        {
+            get { return dob; }
+            set
+            {
+                this.dob = value;
+                RaisePropertyChanged("DOB");
+            }
+        }
+
+        public string IDNum
+        {
+            get { return idNum; }
+            set
+            {
+                this.idNum = value;
+                RaisePropertyChanged("IDNumber");
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                this.lastName = value;
+                RaisePropertyChanged("LastName");
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                this.firstName = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+
+        public SearchInfo(int dob, string idNum, string lastName, string firstName)
+        {
+            this.DOB = dob;
+            this.IDNum = idNum;
+            this.LastName = lastName;
+            this.FirstName = firstName;
+
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void RaisePropertyChanged(string name)
