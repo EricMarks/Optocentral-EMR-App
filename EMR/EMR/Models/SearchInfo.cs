eaea711f4ac56ade31bd6ec0d390451901de4946
elizaebthkuok <elizabethkuok29@gmail.com>
2019-03-11 21:52:17 -0600

    Search Info variables

---
+++ EMR/EMR/Models/SearchInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EMR.Models
+{
+    public class SearchInfo : INotifyPropertyChanged
+    {
+        private int dob;
+        private string idNum;
+        private string lastName;
+        private string firstName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
