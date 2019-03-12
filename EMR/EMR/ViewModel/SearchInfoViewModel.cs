fcc64908c4ad0c9ca615a6264e91e91fcdff4b7b
elizaebthkuok <elizabethkuok29@gmail.com>
2019-03-11 22:07:40 -0600

    Add SearchInfo ViewModel

---
+++ EMR/EMR/ViewModel/SearchInfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using EMR.Models;
+
+namespace EMR.ViewModel
+{
+    public class SearchInfoViewModel
+    {
+        public ObservableCollection<SearchInfo> SearchInfo { get; set; }
+        
+        public SearchInfoViewModel()
+        {
+            this.SearchInfo = new ObservableCollection<SearchInfo>();
+        }
+
+        private void GenerateClients()
+        {
+            SearchInfo.Add(new Models.SearchInfo(0001, "AK00123", "Smith", "Tom"));
+        }
+    }
+}
